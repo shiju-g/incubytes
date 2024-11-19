4d16e2e191a7f06c56a50fa13019fca978a15bdd
shiju-hidragen <157563031+shiju-hidragen@users.noreply.github.com>
2024-11-19 13:30:29 +0530

    title and desc change

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import StringCalculator from "./components/StringCalculator";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen flex items-center justify-center bg-green-50">
       <StringCalculator />
     </div>
   );
